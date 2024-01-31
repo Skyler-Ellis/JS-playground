4e3debdcd9cf5a712667b5bb86b8978dc599fb59
Skyler Ellis <sellis.prsvr@gmail.com>
2024-01-30 21:10:15 -0500

    "started messing"

---
+++ script.js
@@ -0,0 +1,14 @@
+for ( let i = 0; i <= 5; i++){
+    console.log(i)
+}
+
+
+// let animals = ["dogs","cats","birds","fish"];
+// console.log(animals)
+
+
+let animals = ["dogs","cats","birds","fish",
+{type:"lizard", color:"green", name:"henry"}
+];
+    animals.push("rabbit");
+console.log(animals)
